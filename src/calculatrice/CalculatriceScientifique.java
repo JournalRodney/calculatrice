eb0dfe09e3c54566c312b17251ee81e5a2dbbe24
rodjo <rodjo@DESKTOP-T983EQS>
2021-05-20 09:16:31 -0400

    creation de la fenetre

--- src/calculatrice/CalculatriceScientifique.java
+++ src/calculatrice/CalculatriceScientifique.java
@@ -1,5 +1,14 @@
 package calculatrice;
 
+import javax.swing.JFrame;
+
 public class CalculatriceScientifique {
+	
+	CalculatriceScientifique(){
+		JFrame f = new JFrame(" Calculatrice ");
+		f.setSize(450, 650);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
 
 }
--- src/calculatrice/Playbook.java
+++ src/calculatrice/Playbook.java
@@ -1,9 +1,18 @@
 package calculatrice;
 
+import javax.swing.SwingUtilities;
+
+
+
 public class Playbook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new CalculatriceScientifique();
+			}
+		});
 
 	}
 
